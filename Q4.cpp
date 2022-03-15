588d6a54ea5e25280fe2a4818479f7ea70a8fb93
Achintya Mishra <76170002+axhaar@users.noreply.github.com>
2022-03-16 03:39:43 +0530

    Lab 2

---
+++ Q4.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+int pehla(int arr[], int key, int n)
+{
+    int start = 0, end = n - 1, mid;
+    while(start <= end)
+    {
+        mid = (start + end) / 2;
+        if(key > arr[mid])
+            start = mid + 1;
+        else if(key < arr[mid])
+            end = mid;
+        else
+        {
+            if(mid == 0 || arr[mid] != arr[mid - 1])
+                return mid;
+            else
+                end = mid - 1;
+        }
+    }
+    return -1;
+}
+
+int akhiri(int arr[], int key, int n)
+{
+    int start = 0, end = n - 1, mid;
+    while(start <= end)
+    {
+        mid = (start + end) / 2;
+        if(key > arr[mid])
+            start = mid + 1;
+        else if(key < arr[mid])
+            end = mid;
+        else
+        {
+            if(mid == n - 1 || arr[mid] != arr[mid + 1])
+                return mid;
+            else
+                start = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        int key;
+        cin>>key;
+        int first = pehla(arr, key, n);
+        int last = akhiri(arr, key, n);
+        if(first != -1)
+            cout<<key<<" occured "<<(last - first + 1)<<" times\n";
+        else
+            cout<<"Key not found\n";
+    }
+    return 0;
+}
