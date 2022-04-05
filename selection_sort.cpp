66001127d5a79d9cbf09faa9869cf8eff6fb8ff1
Achintya Mishra <76170002+axhaar@users.noreply.github.com>
2022-04-05 14:39:14 +0530

    LabWork

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int arr[] = { 3, 7, 3, 5, 6, 1, 4};
+    int arr[] = { 2, 5, 1, 7, 6, 3};
     int comparisons = 0, swap = 0;
     int size = sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<size-1;i++)
@@ -16,10 +16,13 @@ int main()
             if(arr[j] < arr[min])
                 min = j;
         }
-        swap++;
-        int temp = arr[min];
-        arr[min] = arr[i];
-        arr[i] = temp;
+        if(i != min)
+        {
+            swap++;
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
     }
     for(int i=0;i<size;i++)
         cout<<arr[i]<<" ";
